0022976a111cf8d183b45833a268894015b254b5
Jooones <jonathanV@bitbucket.org>
2018-05-18 01:01:21 +0200

    fix matchIds; add main to test deserialization

--- src/main/java/com/softwaresandbox/pubgclient/api/PubgApiClient.java
+++ src/main/java/com/softwaresandbox/pubgclient/api/PubgApiClient.java
@@ -1,13 +1,19 @@
 package com.softwaresandbox.pubgclient.api;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonDeserializer;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.softwaresandbox.pubgclient.PubgApiClientException;
+import com.softwaresandbox.pubgclient.model.player.PlayersResponse;
 
+import java.time.ZonedDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
 import static com.softwaresandbox.pubgclient.PropertyFileReader.readPubgApiKey;
+import static java.time.ZonedDateTime.parse;
 import static java.util.Arrays.asList;
 import static java.util.stream.Collectors.joining;
 
@@ -41,7 +47,13 @@ public class PubgApiClient {
     // TODO remove
     public static void main(String[] args) {
         try {
-            System.out.println(new PubgApiClient().getPlayers(new HashSet<>(asList("Jooones", "zwiep"))));
+            String players = new PubgApiClient().getPlayers(new HashSet<>(asList("Jooones", "zwiep")));
+            System.out.println(players);
+            GsonBuilder gsonBuilder = new GsonBuilder();
+            gsonBuilder.registerTypeAdapter(ZonedDateTime.class, (JsonDeserializer<ZonedDateTime>) (json, typeOfT, context) -> parse(json.getAsString()));
+            Gson gson = gsonBuilder.create();
+            PlayersResponse playerResponse = gson.fromJson(players, PlayersResponse.class);
+            System.out.println(playerResponse);
         } catch (PubgApiClientException e) {
             e.printStackTrace();
         }
--- src/main/java/com/softwaresandbox/pubgclient/model/player/PlayerRelationships.java
+++ src/main/java/com/softwaresandbox/pubgclient/model/player/PlayerRelationships.java
@@ -1,9 +1,12 @@
 package com.softwaresandbox.pubgclient.model.player;
 
+import com.google.gson.annotations.SerializedName;
 import com.softwaresandbox.pubgclient.model.DataList;
 
 public class PlayerRelationships {
 
+    // TODO find a way to retieve matchIds as a String here without the intermediate object
+    @SerializedName("matches")
     private DataList<MatchId> matchIds;
 
     public DataList<MatchId> getMatchIds() {
