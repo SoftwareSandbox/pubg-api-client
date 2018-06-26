d52470dc138c5c76f4a571c0c38ef278f3842028
Jooones <jonathanV@bitbucket.org>
2018-06-26 21:02:45 +0200

    ignore integrationtest for now as travis doesnt have the key for contacting the api

--- src/test/java/be/swsb/pubgclient/PubgApiClientIntegrationTest.java
+++ src/test/java/be/swsb/pubgclient/PubgApiClientIntegrationTest.java
@@ -1,6 +1,7 @@
 package be.swsb.pubgclient;
 
 import be.swsb.pubgclient.model.player.PlayerResponse;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.Optional;
@@ -9,8 +10,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class PubgApiClientIntegrationTest {
 
+    // TODO add key to travis and make sure it works locally as well and then remove @ignore from here
     @Test
     @SuppressWarnings("ConstantConditions")
+    @Ignore
     public void getPlayerByNameReturnsExpectedResult() throws PubgApiClientException {
         Optional<PlayerResponse> playerResponse = new PubgApiClientBuilder().build().getPlayerByName("shroud", "pc-na");
 
